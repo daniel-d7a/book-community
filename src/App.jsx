d2d402076076f4e3bf481f2cd7a3e52eb66647d1
ehabmohamed <ehab20191700168@cis.asu.edu.eg>
2023-04-06 23:04:56 +0200

    Fixed previous commit issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Post from "./Components/Post/Post";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const user = {
+    username: "Ehab Mohamed",
+    state: "w"
+  }
 
   return (
     <>
-      
+      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
+      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
+      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
     </>
   );
 }
