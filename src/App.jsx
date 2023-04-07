d1b0b7d53486264c9177d37cfa9650c53e8be96c
ehabmohamed <ehab20191700168@cis.asu.edu.eg>
2023-04-07 11:22:26 +0200

    Created classes and functionalities to voters

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
 import { useState } from "react";
 import Post from "./Components/Post/Post";
 
-function App() {
-  const user = {
-    username: "Ehab Mohamed",
-    state: "w"
+class User{
+  constructor(username, state, profile,starred = [] ){
+    this.username = username
+    this.state = state
+    this.starred = starred
+    this.profile = profile
   }
 
+  addStarred(post){
+    this.starred.push(post)
+  }
+}
+
+class PostObj{
+  constructor(user, community, date, text, image, location = null,commments = [], votes = 0){
+    this.user = user
+    this.community = community
+    this.location = location
+    this.date = date
+    this.text = text
+    this.image = image
+    this.votes = votes
+    this.comments = commments
+  }
+
+  setVotes(vote){
+    this.votes += vote
+  }
+  setLocation(loc){
+    this.location = loc
+  }
+
+  addComment(comment){
+    this.comments.push(comment)
+  }
+
+}
+
+
+
+
+function App() {
+  const user = new User("Ehab Mohamed","r","https://spunout.ie/wp-content/uploads/2021/01/portrait-black-young-man-face-man-person-ethnic-student-diversity-diverse-confident-millennial_t20_K6aZOV-2.jpg")
+  const post = new PostObj(user, "Head First Javascript",30,"best book about javascript, hands down, would very much recommend for any one who is just starting out.","https://www.flenov.info//pics/4a5/4077-HeadFirstJavaScript.jpg","Cairo, Egypt",[""],14)
   return (
     <>
-      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
-      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
-      <Post user = {user} community={"Head First Javascript"} text={"best book about javascript, hands down, would very much recommend for any one who is just starting out."} reacts={10} comments={15}/>
+      <Post user = {user} post={post} />
     </>
   );
 }
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,21 +1,55 @@
 import {TfiLocationPin} from "react-icons/tfi"
+import { useState } from "react"
 import {BiUpvote,BiDownvote, BiCommentDetail,BiShareAlt,BiStar,BiDotsHorizontalRounded,BiBookReader,BiEditAlt} from "react-icons/bi"
-export default function Post({user, community , text, reacts, comments}) {
-
+export default function Post({user, post}) {
+    const[votes, setVotes] = useState(post.votes)
+    const[upVoted,setUpvoted] = useState(false)
+    const[downVoted,setDownvoted] = useState(false)
+    function handleclick(amount){
+        if(!upVoted && !downVoted){
+            if (amount===1){
+                setUpvoted(true)
+            } else if(amount === -1){
+                setDownvoted(true)
+                
+            }
+            post.setVotes(amount)
+            setVotes(post.votes)
+        } else if(upVoted && amount ===1){
+            setUpvoted(false)
+            post.setVotes(-amount)
+            setVotes(post.votes)
+        } else if(upVoted && amount === -1){
+            setUpvoted(false)
+            setDownvoted(true)
+            post.setVotes(2*amount)
+            setVotes(post.votes)
+        } else if(downVoted && amount === -1){
+            setDownvoted(false)
+            post.setVotes(-amount)
+            setVotes(post.votes)
+        } else if(downVoted && amount === 1){
+            setUpvoted(true)
+            setDownvoted(false)
+            post.setVotes(2*amount)
+            setVotes(post.votes)
+        }
+        
+    }
     return(<>
         <div className="card w-full bg-base-100 shadow-xl mb-2">
         <div className="flex gap-4 items-center pt-4 pl-4">
             <div className="relative">
-                <img src="https://spunout.ie/wp-content/uploads/2021/01/portrait-black-young-man-face-man-person-ethnic-student-diversity-diverse-confident-millennial_t20_K6aZOV-2.jpg" alt="Shoes" className=" w-12 h-12 rounded-full object-cover" />
+                <img src={user.profile} alt="Shoes" className=" w-12 h-12 rounded-full object-cover" />
                 <div className={`absolute bottom-0 right-0 w-5 h-5 rounded-full ${user.state ==="r"? "bg-teal-500": "bg-yellow-500"}  text-black border-2 border-base-100 flex items-center justify-center text-[14px]`}>
                     {user.state === "r"? <BiBookReader/> : <BiEditAlt/>}
                 </div>
             </div>
             <div className="text-sm">
-                <a href="#">{user.username || "user"} / <a href="#">{community.length > 15? community.substring(0,15) + '...' : community}</a></a>
+                <a href="#">{user.username || "user"} / <a href="#">{post.community.length > 15? post.community.substring(0,15) + '...' : post.community}</a></a>
                 <div className="flex gap-2 text-xs text-zinc-400">
-                    <p>30 minutes ago</p>
-                    <a href="#" className="flex gap-1 items-center"><TfiLocationPin/> Cairo, Egypt</a>
+                    <p>{post.date} minutes ago</p>
+                    <a href="#" className="flex gap-1 items-center"><TfiLocationPin/> {post.location}</a>
                 </div>
             </div>
             
@@ -31,14 +65,14 @@ export default function Post({user, community , text, reacts, comments}) {
             </div>
         </div>
         <div className="pl-4 pt-4">
-            <p>{text}</p>
+            <p>{post.text}</p>
         </div>
         <figure className="px-4 pt-4">
-            <img src="https://www.flenov.info//pics/4a5/4077-HeadFirstJavaScript.jpg" alt="Shoes" className="" />
+            <img src={post.image} className="" />
         </figure>
         <div className="flex gap-4 items-center px-4 py-2">
-            <p className="flex gap-2 items-center"><BiUpvote/> {reacts} <BiDownvote/></p>
-            <p className="flex gap-2 items-center"><BiCommentDetail/> {comments}</p>
+            <p className="flex gap-2 items-center"><BiUpvote className={`${upVoted? "text-teal-500":"text-white"}`} onClick={()=>{handleclick(1)}}/> {votes} <BiDownvote className={`${downVoted? "text-yellow-500":"text-white"}`} onClick={()=>{handleclick(-1)}}/></p>
+            <p className="flex gap-2 items-center"><BiCommentDetail/> {post.comments.length}</p>
             <BiShareAlt/>
             <BiStar className="ml-auto"/>
         </div>
