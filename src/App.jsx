d91ad1d23f1f8030f1481de6e4f94fba07850952
ehabmohamed <ehab20191700168@cis.asu.edu.eg>
2023-04-09 09:57:35 +0200

    Finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Post from "./Components/Post/Post";
+import Nav from "./Components/Nav/Nav";
 
 class User{
   constructor(username, state, profile,starred = [] ){
@@ -43,11 +44,12 @@ class PostObj{
 
 
 function App() {
-  const user = new User("Ehab Mohamed","r","https://spunout.ie/wp-content/uploads/2021/01/portrait-black-young-man-face-man-person-ethnic-student-diversity-diverse-confident-millennial_t20_K6aZOV-2.jpg")
+  const user = new User("Ehab Mohamed","w","https://spunout.ie/wp-content/uploads/2021/01/portrait-black-young-man-face-man-person-ethnic-student-diversity-diverse-confident-millennial_t20_K6aZOV-2.jpg")
   const post = new PostObj(user, "Head First Javascript",30,"best book about javascript, hands down, would very much recommend for any one who is just starting out.","https://www.flenov.info//pics/4a5/4077-HeadFirstJavaScript.jpg","Cairo, Egypt",[""],14)
   return (
     <>
       <Post user = {user} post={post} />
+      <Nav/>
     </>
   );
 }
