0f9235772e95cd2a499718d227ec65d519da057b
daniel-d7a <eyad.taym.saba@gmail.com>
2023-07-01 17:33:08 +0300

    added notifications section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import CreateFeed from "./Components/Feed/CreateFeed";
+import Notifications from "./Components/Notifications/Notifications";
 import Profile from "./Components/profile/Profile";
 
 function App() {
 
   return (
     <>
+      <Notifications/>
       {/* <CreateFeed/> */}
-      <Profile/>
+      {/* <Profile/> */}
     </>
   );
 }
