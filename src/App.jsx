e50e5e5706222a4e32409e29f75793192a0be0f3
Ehab Mohamed <ihabmohamed6699@gmail.com>
2023-07-02 16:54:45 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import Home from "./Components/Home/Home";
 import Post from "./Components/Post/Post";
 import Nav from "./Components/Nav/Nav";
 import CreateFeed from "./Components/Feed/CreateFeed";
 import Notifications from "./Components/Notifications/Notifications";
 import Profile from "./Components/profile/Profile";
+import { Route, Routes } from "react-router-dom";
 
 
 class User{
@@ -62,14 +63,21 @@ class Reply{
 }
 
 function App() {
+  
   const user = new User(1,"Ehab Mohamed","w","https://spunout.ie/wp-content/uploads/2021/01/portrait-black-young-man-face-man-person-ethnic-student-diversity-diverse-confident-millennial_t20_K6aZOV-2.jpg")
   const reply = new Reply(1,user,"I agreeeeee")
   const comment = new Comment(1,user,"Yeah I Like this book", Array(2).fill(reply))
   const comments = Array(15).fill(comment)
   const post = new PostObj(1,user, "Head First Javascript",30,"best book about javascript, hands down, would very much recommend for any one who is just starting out.","https://www.flenov.info//pics/4a5/4077-HeadFirstJavaScript.jpg","Cairo, Egypt",comments,14)
+  const posts = Array(5).fill(post)
   return (
     <>
-      <Post user = {user} post={post} />
+      <div className="pb-14">
+        <Routes>
+          <Route path="/" element={<Home feed={posts}/>}/>
+          <Route path="/notifications" element={<Notifications/>}/>
+        </Routes>
+      </div>
       <Nav/>   
     </>
   );
